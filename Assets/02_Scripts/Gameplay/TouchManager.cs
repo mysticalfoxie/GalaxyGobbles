b53314f7fb8671550a5618e3d0ea014f11493f51
Zarbony <jenskaerger95@gmail.com>
2024-04-09 16:54:38 +0200

    Delete PlayerMovement Add TouchManager + Touch Movement Add TouchPressInput & Position [WIP TouchPositionChange]

---
+++ Assets/02_Scripts/Gameplay/TouchManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    private PlayerInput _playerInput;
+    private InputAction _touchPressAction;
+    private InputAction _touchPosition;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _touchPressAction = _playerInput.actions["TouchInteract"];
+        _touchPosition = _playerInput.actions["TouchPosition"];
+    }
+
+    private void OnEnable()
+    {
+        _touchPressAction.performed += _touchPressed;
+    }
+
+    private void OnDisable()
+    {
+        _touchPressAction.performed -= _touchPressed;
+    }
+
+    private void _touchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(_touchPosition.ReadValue<Vector2>());
+        position.z = _player.transform.position.z;
+        _player.transform.position = position;
+
+    }
+}
