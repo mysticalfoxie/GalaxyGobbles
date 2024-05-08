d7545aea2e230659d4e89d4886d50f6959ea5262
Zarbony <jenskaerger95@gmail.com>
2024-05-08 14:00:06 +0200

    Prepare new Layout and Star Indicator

--- Assets/02_Scripts/UI/Progressbar.cs
+++ Assets/02_Scripts/UI/Progressbar.cs
@@ -1,35 +1,52 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Progressbar : MonoBehaviour
 {
     public static int Progress;
-    
+
     public static Progressbar Instance { get; set; }
 
-    public void OnClickZeroStar()
-    {
-        Progress = 0;
-        Debug.Log($"SetStars to {Progress}");
-    }
-    public void OnClickOneStar()
-    {
-        Progress = 1;
-        Debug.Log($"SetStars to {Progress}");
+    [SerializeField] private Slider _progressSlider;
+    [SerializeField] [Range(0, 100)] private int _starOneReached;
+    [SerializeField] [Range(0, 100)] private int _starTwoReached;
+    [SerializeField] [Range(0, 100)] private int _starThreeReached;
 
-    }
-    
-    public void OnClickTwoStar()
-    {
-        Progress = 2;
-        Debug.Log($"SetStars to {Progress}");
 
-    }
-    
-    public void OnClickThreeStar()
+    private void Awake()
     {
-        Progress = 3;
-        Debug.Log($"SetStars to {Progress}");
-
+        if (!_progressSlider) _progressSlider = gameObject.GetComponentInChildren<Slider>();
     }
-    
+
+    private void Update()
+    {
+        if (_progressSlider.value >= _starOneReached)
+        {
+            Progress = 1;
+            Debug.Log("You reached Star 1!");
+        }
+        else if (_progressSlider.value >= _starTwoReached)
+        {
+            Progress = 2;
+            Debug.Log("You reached Star 2!");
+        }
+        else if (_progressSlider.value >= _starThreeReached)
+        {
+            Progress = 3;
+            Debug.Log("You reached Star 3!");
+        }
+    }
+
+    public void ProgressSlider(int sliderValue)
+    {
+        _progressSlider.value++;
+    }
+
+    public void OnClickFillProgressbar()
+    {
+        _progressSlider.value++;
+        Debug.Log($"Set Progressbar to {_progressSlider.value}");
+    }
 }
