98d7e3e83138c96db53de0162fc00a8712d7601e
MysticalFoxie <michellebecker2003@gmail.com>
2024-06-19 14:31:15 +0200

    Fix Ikaruz positioning for new tables

--- Assets/02_Scripts/Gameplay/Tables/Table.cs
+++ Assets/02_Scripts/Gameplay/Tables/Table.cs
@@ -39,12 +39,25 @@ public class Table : Touchable
     public void Seat(Customer customer, Chair chair = null)
     {
         chair ??= _chairs.First();
-        var position = chair.transform.position + customer.Data.Species.ChairOffsetHorizontal;
-        customer.transform.position = position;
+        var position = chair.transform.position;
+        var offset = customer.Data.Species.ChairOffsetHorizontal;
+        if (chair.Side == Direction.Right) 
+            offset = new Vector3(offset.x * -1.0F, offset.y, offset.z); 
+        
+        customer.transform.position = position + offset;
         Customer = customer;
         Customer.Table = this;
         Customer.Chair = chair;
     }
+    /*
+     * Before
+     * UnityEditor.TransformWorldPlacementJSON:{"position":{"x":1252.0400390625,"y":18.527524948120118,"z":-101.66825103759766},"rotation":{"x":0.2588191032409668,"y":0.0,"z":0.0,"w":0.9659258127212524},"scale":{"x":0.17376121878623963,"y":0.16284774243831635,"z":1.0}}
+     *
+     * After
+     * UnityEditor.TransformWorldPlacementJSON:{"position":{"x":1263.800048828125,"y":47.400001525878909,"z":-110.19999694824219},"rotation":{"x":0.2588191032409668,"y":0.0,"z":0.0,"w":0.9659258127212524},"scale":{"x":0.17376121878623963,"y":0.16284774243831635,"z":1.0}}
+     *
+     * 
+     */
 
     public void SetDirty()
     {
