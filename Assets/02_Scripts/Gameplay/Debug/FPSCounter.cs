73e4ed74bdbc93a25b6b1bf7ddd79d8532b1299c
MysticalFoxie <michellebecker2003@gmail.com>
2024-08-06 17:10:25 +0200

    Get the last bit of FPS out there

--- Assets/02_Scripts/Gameplay/Debug/FPSCounter.cs
+++ Assets/02_Scripts/Gameplay/Debug/FPSCounter.cs
@@ -12,6 +12,7 @@ public class FPSCounter : Singleton<FPSCounter>
     [SerializeField] private float _capturingDelay = 1.0F;
     [SerializeField] private bool _displayDelta;
 
+#if UNITY_EDITOR
     public override void Awake()
     {
         if (!Debug.isDebugBuild) return;
@@ -24,6 +25,12 @@ public class FPSCounter : Singleton<FPSCounter>
         StartCoroutine(nameof(StartSecondTicks));
         Instance.gameObject.SetActive(false);
     }
+#else
+    public void Awake()
+    {
+        Destroy(gameObject);
+    }
+#endif
 
     public void Update()
     {
